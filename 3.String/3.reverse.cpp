b5cb6c05d2fdb89c1a7b4bc8fa8fb5fa2c22695e
Aman-webDev-2025 <mr.aman.kr2025@gmail.com>
2026-07-16 17:15:59 +0530

    reverse string

--- 3.String/3.reverse.cpp
+++ 3.String/3.reverse.cpp
@@ -4,14 +4,35 @@
 #include<string>
 using namespace std;
 
+// int main(){
+//     string s;
+//     cout<<"enter a string : ";
+//     getline(cin , s);
+
+//     for(int i=s.length()-1 ; i>=0 ; i--){
+//         cout<<s[i];
+//     }
+
+//     return 0;
+// }
+
+
+
 int main(){
     string s;
-    cout<<"enter a string : ";
     getline(cin , s);
-
-    for(int i=s.length()-1 ; i>=0 ; i--){
-        cout<<s[i];
+    int index =-1;
+    for(int i=0 ; i<=s.length() ; i++){
+        if(i == s.length() || s[i] == ' '){
+            int start = index + 1;
+            int end = i-1;
+            while(start < end){
+                swap(s[start] , s[end]);
+                start++;
+                end--;
+            }
+            index = i;
+        }
     }
-
-    return 0;
+    cout<<s;
 }
